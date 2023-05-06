7f73414848f2c16793269df238a83a3b0a04275a
Pablo Ortiz <pablo.ortiz.barra@gmail.com>
2023-05-05 20:52:37 -0400

    Guarda el token de correo

--- app.js
+++ app.js
@@ -66,11 +66,10 @@ const simularlogin = async () =>{
 
     await loginInput.type("pablo.ortiz.barra@gmail.com");
 
-    await loginPassword.type("Emt.532%");
+    await loginPassword.type("Emt.532%"); 
 
     await page.click("button[type=submit]"); //login enter click por class css
 
-   
     await page.screenshot({ path: 'example.png'}); //screenshot de la pagina
   
     const cookies = await page.cookies();
@@ -95,12 +94,22 @@ const simularlogin = async () =>{
 
           
         }
-
-   console.log(user);
-   fs.writeFile('usuario.txt', JSON.stringify(user), (err) =>{
-    if (err) return console.error(err);
-});
+    //guardo en un achivo los datos de usuario obtenidos   
+    console.log(user);
+    fs.writeFile('usuario.txt', JSON.stringify(user), (err) =>{
+        if (err) return console.error(err);
+    });
+     //guardo en un achivo los datos de usuario obtenidos
+    let token = await page.evaluate(() => {
+        return localStorage.getItem("tokenEmail");
+      });
+    console.log(token);
+    fs.writeFile('token.txt', JSON.stringify(token), (err) =>{
+        if (err) return console.error(err);
+    });
     await page.screenshot({ path: 'datos.png'});
+
+    await browser.close();
    
 
 }
